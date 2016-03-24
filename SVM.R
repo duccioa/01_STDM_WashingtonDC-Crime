b211ede60eb6944a12d3290ca67be0287a91bf54
duccioa <duccio.aiazzi@gmail.com>
2016-03-24 11:44:24 +0000

    SVM appears to work

--- SVM.R
+++ SVM.R
@@ -3,9 +3,9 @@ load('./data/final_sp.RData')
 # SVM
 yrs_training = c(2011, 2012, 2013)
 yr_test = 2014
-season_test = 1 ; season = 'WINTER'# 1 for WINTER, 2 for SPRING, 3 for SUMMER, 4 for AUTUMN
-off = 'THEFT/OTHER'
-dc@data = dc@data[complete.cases(dc@data),]
+season_test = 2 ; season = 'SUMMER'# 1 for WINTER, 2 for SPRING, 3 for SUMMER, 4 for AUTUMN
+off = 'BURGLARY'
+#dc@data = dc@data[complete.cases(dc@data),]
 n = 1:nrow(dc@data)
 trainInd = n[dc@data$OFFENSE %in% off & dc@data$YEAR %in% yrs_training]
 yTestInd = n[dc@data$OFFENSE %in% off & dc@data$YEAR %in% yr_test & dc@data[,season] == 1]
@@ -14,31 +14,34 @@ dc@data$LABEL = ifelse(dc@data$Count <= m, -1, 1)
 X = as.matrix(dc@data[,-c(1,2,3,4,5, ncol(dc@data))])
 y = as.matrix(dc@data$LABEL)
 XTrain = X[trainInd,]
-XTest = X[-trainInd,]; XTest = XTest[XTest[,season_test] == 1,]
+XTest = X[yTestInd,]
 yTrain = y[trainInd]
 yTest = y[yTestInd]
 
 
 # SVM test 1
 svm_type = "C-svc"
-c_value = 1
-kernel = 'rbfdot'
-lsModel <- ksvm(x=XTrain, y=yTrain,type=svm_type)
+c_value = 100
+ker = 'polydot' # polydot rbfdot
+kp = 'automatic'
+lsModel <- ksvm(x=XTrain, y=yTrain,type=svm_type, kernel = ker, kpar = kp, C = c_value)
 lsPred <- predict(lsModel, XTest)
-
-
-
+predErr <- length(which((lsPred-yTest)>0))/length(lsPred)
 
 pltClass <- lsModel@fitted
 pltClass[lsModel@SVindex] <- 2
 brks <- c(-1,1,2)
 lbls <- findInterval(pltClass, brks)
-col_palette <- c("plum1", "grey", "orangered")
-plot(dc[-trainInd,], col=cols[lbls])
+col_palette <- c("orangered", "grey", "plum1")
+png(paste0('./Figures/SVM_', gsub('/', '-', off), '_', season, '_C', as.character(c_value), '_', ker, '_', kp, '.png'),
+           width = 1200, height = 1000)
+plot(dc[-yTestInd,], col=cols[lbls], lwd = .5)
 title(main = paste0('SVM Classification results: ', off), sub = paste0('Washington DC, ', season), 
-      cex.main = 1, cex.sub = .8)
-legend('bottomright', legend = c('Above median', 'Support Vectors','Below median'), fill = col_palette, bty="n", cex = .8, title = '')
-
-
-
+      cex.main = 2, cex.sub = 1.5)
+legend('bottomright', legend = c('Above median', 'Support Vectors','Below median'), fill = col_palette, bty="n", cex = 1.5, title = '')
+text(x = -76.90461, y = 38.985, labels = paste('C = ', c_value), cex = 1.5, pos = 4)
+text(x = -76.90461, y = 38.97, labels = paste('Kernel = ', ker), cex = 1.5, pos = 4)
+text(x = -76.90461, y = 38.955, labels = paste('Kpar = ', kp), cex = 1.5, pos = 4)
+text(x = -76.90461, y = 38.93, labels = paste('Error = ', round(predErr, 3)), cex = 1.5, pos = 4)
+dev.off()
 
